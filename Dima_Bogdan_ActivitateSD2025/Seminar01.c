357dfebf00749970d99037fa8a2430c75ace13bd
bogdan <bogdan.k983@gmail.com>
2025-03-15 21:24:17 +0200

    Citirea unui numar intreg

--- Dima_Bogdan_ActivitateSD2025/Seminar01.c
+++ Dima_Bogdan_ActivitateSD2025/Seminar01.c
@@ -1,6 +1,10 @@
 #include<stdio.h>
 
 int main() {
-	printf("Salutare");
+	printf("Salutare\n Introduceti un numar intreg:");
+	int variabila = 0;
+	scanf_s("%d", &variabila);
+	printf("Ai introdus: %d", variabila);
+
 	return 0;
 }
