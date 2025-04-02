521bdeeaf32d43a88a8c611d9621bda125bc15ad
bogdan <bogdan.k983@gmail.com>
2025-04-02 13:22:33 +0300

    Functia pentru citirea unui obiect de tip Bicileta

--- Dima_Bogdan_ActivitateSD2025/task1.c
+++ Dima_Bogdan_ActivitateSD2025/task1.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -9,3 +9,27 @@ typedef struct {
     int nrViteze;
     float* preturi;
 } Bicicleta;
+// Funcție pentru citirea unui obiect de tip Bicicleta
+Bicicleta citireBicicleta() {
+    Bicicleta b;
+    printf("Introdu codul: ");
+    scanf("%d", &b.cod);
+
+    char buffer[100];
+    printf("Introdu brandul: ");
+    scanf("%s", buffer);
+    b.brand = (char*)malloc((strlen(buffer) + 1) * sizeof(char));
+    strcpy(b.brand, buffer);
+
+    printf("Introdu numarul de viteze: ");
+    scanf("%d", &b.nrViteze);
+
+    b.preturi = (float*)malloc(b.nrViteze * sizeof(float));
+    printf("Introdu pretul pentru fiecare model: \n");
+    for (int i = 0; i < b.nrViteze; i++) {
+        printf("Model %d: ", i + 1);
+        scanf("%f", &b.preturi[i]);
+    }
+
+    return b;
+}
