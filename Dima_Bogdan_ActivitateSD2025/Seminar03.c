04a41f82495766b0e52edc3d54713045a028f353
bogdan <bogdan.k983@gmail.com>
2025-04-02 13:20:11 +0300

    creearea structurii bicicleta cu 2 litere din numele Bogdan Dima (BD)

--- Dima_Bogdan_ActivitateSD2025/Seminar03.c
+++ Dima_Bogdan_ActivitateSD2025/Seminar03.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
-
-int main() {
-	printf("Introduceti un numar real:");
-	float variabila = 0.0;
-	scanf_s("%f", &variabila);
-	printf("Ati introdus: %5.2f", variabila);
-	
-
-}
+//#include<stdio.h>
+//
+//int main() {
+//	printf("Introduceti un numar real:");
+//	float variabila = 0.0;
+//	scanf_s("%f", &variabila);
+//	printf("Ati introdus: %5.2f", variabila);
+//	
+//
+//}
---
+++ Dima_Bogdan_ActivitateSD2025/task1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// Definirea structurii Bicicleta
+typedef struct {
+    int cod;
+    char* brand;
+    int nrViteze;
+    float* preturi;
+} Bicicleta;
