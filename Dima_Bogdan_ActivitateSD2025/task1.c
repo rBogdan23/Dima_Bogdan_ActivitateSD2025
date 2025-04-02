42a70fb211047bd6cf338ac78f24888730818dd5
bogdan <bogdan.k983@gmail.com>
2025-04-02 13:46:53 +0300

    Functie pt calcul pret mediu

--- Dima_Bogdan_ActivitateSD2025/task1.c
+++ Dima_Bogdan_ActivitateSD2025/task1.c
@@ -13,23 +13,33 @@ typedef struct {
 Bicicleta citireBicicleta() {
     Bicicleta b;
     printf("Introdu codul: ");
-    scanf("%d", &b.cod);
+    scanf_s("%d", &b.cod);
 
     char buffer[100];
     printf("Introdu brandul: ");
-    scanf("%s", buffer);
+    scanf_s("%s", buffer);
     b.brand = (char*)malloc((strlen(buffer) + 1) * sizeof(char));
     strcpy(b.brand, buffer);
 
     printf("Introdu numarul de viteze: ");
-    scanf("%d", &b.nrViteze);
+    scanf_s("%d", &b.nrViteze);
 
     b.preturi = (float*)malloc(b.nrViteze * sizeof(float));
     printf("Introdu pretul pentru fiecare model: \n");
     for (int i = 0; i < b.nrViteze; i++) {
         printf("Model %d: ", i + 1);
-        scanf("%f", &b.preturi[i]);
+        scanf_s("%f", &b.preturi[i]);
     }
 
     return b;
+
 }
+// Funcție pentru calculul pretului mediu
+float pretMediu(Bicicleta b) {
+    float suma = 0;
+    for (int i = 0; i < b.nrViteze; i++) {
+        suma += b.preturi[i];
+    }
+    return (b.nrViteze > 0) ? suma / b.nrViteze : 0;
+}
+
